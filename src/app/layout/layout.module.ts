81dc8a0a0f57ade908c36fbcddc54ac8118d4b58
Francisco <franciscopinto90@gmail.com>
2019-08-06 17:34:35 +0100

    Iniciar Teste routing done

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -20,7 +20,8 @@ import { IniciarTesteComponent } from './iniciar-teste/iniciar-teste.component';
     ConsultarResultadosComponent,
     ConsultarEnunciadosComponent,
     CriarTestesComponent,
-    SettingsComponent
+    SettingsComponent,
+    IniciarTesteComponent
   ],
   imports: [
     CommonModule,
