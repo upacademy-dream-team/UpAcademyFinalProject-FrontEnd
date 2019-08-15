d92a02dec7d4682e0b8e9de87ce72b51a4e680c9
FranciscoP1nto <51887651+FranciscoP1nto@users.noreply.github.com>
2019-08-15 19:08:48 +0100

    admin table filter working - menu done

--- src/app/layout/shared/component/ngx-table/ngx-table.component.ts
+++ src/app/layout/shared/component/ngx-table/ngx-table.component.ts
@@ -15,39 +15,30 @@ export class NgxTableComponent implements OnInit {
 
   @Input() rows: any;
   @Input() columns: any;
+  @Input() temp: any;
   @Output() clickedRow = new EventEmitter();
   @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
-  filteredData = [];
 
-  constructor( 
+  constructor(
     private userApi: UserServiceService,
-    private modalService: NgbModal,) {
+    private modalService: NgbModal) {
   }
 
 
   ngOnInit() {
-    this.filteredData = this.rows;
   }
 
-  filterDatatable(event){
-    // get the value of the key pressed and make it lowercase
-    let val = event.target.value.toLowerCase();
-    // get the amount of columns in the table
-    let colsAmt = this.columns.length;
-    // get the key names of each column in the dataset
-    let keys = Object.keys(this.rows[0]);
-    // assign filtered matches to the active datatable
-    this.rows = this.filteredData.filter(function(item){
-      // iterate through each row's column data
-      for (let i=0; i<colsAmt; i++){
-        // check for a match
-        if (item[keys[i]].toString().toLowerCase().indexOf(val) !== -1 || !val){
-          // found match, return true to add to result set
-          return true;
-        }
-      }
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+      return d.username.toLowerCase().indexOf(val) !== -1 || !val;
     });
-    // whenever the filter changes, always go back to the first page
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
     this.table.offset = 0;
   }
 
