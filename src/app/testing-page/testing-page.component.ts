e931c48b76e99f7b96f759f305a573ef30ff6b13
anthonygduarte <52419234+anthonygduarte@users.noreply.github.com>
2019-08-19 17:49:06 +0100

    Solved Test

--- src/app/testing-page/testing-page.component.ts
+++ src/app/testing-page/testing-page.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TestServiceService } from '../core/services/test-service/test-service.service';
 import { ReplaySubject, Subscription } from 'rxjs';
 import { User } from '../core';
+import { Answer } from '../core/models/answer';
+import { Candidate } from '../core/models/candidate';
+import { SolvedTest } from '../core/models/solvedTest';
+import { SolvedTestServiceService } from '../core/services/solvedTest-service/solved-test-service.service';
 
 @Component({
   selector: 'app-testing-page',
@@ -13,9 +17,15 @@ export class TestingPageComponent implements OnInit, OnDestroy {
   private subscriptionTest: Subscription;
   private currentTest: number = 1;
   private testRunning = 0;
+  private test;
+  private answer=[];
+  private candidate= new Candidate();
+  private emailRecruiter="admin@admin.pt";
+  private solvedTest = new SolvedTest();
 
   constructor(
-    private testService: TestServiceService) {
+    private testService: TestServiceService,
+    private solvedService: SolvedTestServiceService) {
       this.test$ = this.testService.test$;
       this.subscriptionTest = this.test$.subscribe((a) => console.log('test$', JSON.stringify(a)));
      }
@@ -25,6 +35,7 @@ export class TestingPageComponent implements OnInit, OnDestroy {
     console.log(this.currentTest);
     this.testService.getTest(this.currentTest);
     this.currentTestCheck();
+    this.test$.subscribe(data=> {this.test=data; console.log("o meu teste");this.initiateAnswersObject(this.test)});
   }
 
   ngOnDestroy() {
@@ -39,4 +50,34 @@ export class TestingPageComponent implements OnInit, OnDestroy {
     this.testRunning = 2;
 
   }
+
+  initiateAnswersObject(test){
+    let numberOfQuestions=test.questions.length;
+    console.log(numberOfQuestions);
+    for(let i=0; i<numberOfQuestions; i++){
+        this.answer.push({questionID: test.questions[i].id, givenAnswer:[]});
+    }
+    console.log(this.answer);
+  }
+
+  onChange(indexSelection:number, indexQuestion:number, isChecked: boolean) {
+    if(isChecked) {
+      this.answer[indexQuestion].givenAnswer.push(indexSelection);
+    } else {
+      this.answer[indexQuestion].givenAnswer.splice(this.answer.indexOf(indexSelection),1);
+    }
+    console.log(this.answer[indexQuestion]);
+  }
+
+  submitTest(){
+    this.candidate.emailRecruiter=this.emailRecruiter;
+    this.solvedTest.answer=this.answer;
+    this.solvedTest.candidate=this.candidate;
+    this.solvedTest.timeSpent=10;
+    this.solvedTest.testID=this.test.id;
+    console.log(JSON.stringify(this.solvedTest));
+    this.solvedService.addSolvedTest(this.solvedTest).subscribe(data=> console.log(data), error=>console.log(error.error));
+    console.log("done");
+  }
+
 }
