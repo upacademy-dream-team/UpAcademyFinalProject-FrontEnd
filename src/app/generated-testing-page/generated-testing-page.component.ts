9196e09035d65277c4be7c6624602a2632356b7e
anthonygduarte <52419234+anthonygduarte@users.noreply.github.com>
2019-08-26 13:00:57 +0100

    Timer adjusted

--- src/app/generated-testing-page/generated-testing-page.component.ts
+++ src/app/generated-testing-page/generated-testing-page.component.ts
@@ -18,6 +18,7 @@ export class GeneratedTestingPageComponent implements OnInit, OnDestroy {
   private candidate= new Candidate();
   private solvedTest = new SolvedTest();
   private startTime;
+  private diff;
   private finishTime;
   private optionLetter: string;
   private answer=[];
@@ -65,7 +66,7 @@ export class GeneratedTestingPageComponent implements OnInit, OnDestroy {
     if(this.session.candidateEmail!=this.candidate.email)
       this.emailValid=false;
     else{
-      this.sessionService.beginSession(this.session.sessionID).subscribe(data => console.log(data), error=> console.log(error.error));
+      this.sessionService.beginSession(this.session.sessionID).subscribe(data => this.diff=data, error=> console.log(error.error));
       this.startTime=new Date();
       this.testRunning = 2;
     }
