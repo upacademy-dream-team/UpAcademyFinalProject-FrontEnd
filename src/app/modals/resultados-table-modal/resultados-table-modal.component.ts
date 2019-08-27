04aa6a007fe00c9e9ffc699c1f89646c18e31969
anthonygduarte <52419234+anthonygduarte@users.noreply.github.com>
2019-08-27 19:00:17 +0100

    test - solved test - consultar enunciados

--- src/app/modals/resultados-table-modal/resultados-table-modal.component.ts
+++ src/app/modals/resultados-table-modal/resultados-table-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { UserServiceService } from 'src/app/core';
 import { TestServiceService } from 'src/app/core/services/test-service/test-service.service';
+import { SolvedTestServiceService } from 'src/app/core/services/solvedTest-service/solved-test-service.service';
 
 @Component({
   selector: 'app-resultados-table-modal',
@@ -14,10 +15,13 @@ export class ResultadosTableModalComponent implements OnInit {
   @Input()check: number;
   @Input() messageMsg;
   @Input() messageTestName;
+  @Input() id;
   @Input() TestQuestions;
   @Input() TestAnswers;
   @Input() messageTestTotal;
   public myTest;
+  public allData;
+  public enunciado;
 
   @Output() passEntry: EventEmitter<any> = new EventEmitter();
 
@@ -25,9 +29,21 @@ export class ResultadosTableModalComponent implements OnInit {
     private modalService: NgbModal,
     private userApi: UserServiceService,
     private testService: TestServiceService,
+    private solvedService: SolvedTestServiceService,
     ) {}
 
   ngOnInit() {
+    
+    this.solvedService.getSolvedTest(1).subscribe(data=> 
+      { this.allData=data; 
+        this.myTest=this.allData.solvedTest, 
+        console.log(this.myTest);
+        let ID=this.myTest.testID;
+        this.testService.getTestByID(ID).subscribe(data=>{
+          this.enunciado=data; 
+          console.log(this.enunciado);
+        });
+      });
     /* console.log(this.messageTestTotal.id);
     this.testService.getTestWithSolutions(this.messageTestTotal.id).subscribe(data=> {this.myTest=data; console.log(data)}); */
   }
